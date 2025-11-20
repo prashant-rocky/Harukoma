33462100eda48af6d020321e28b34363ee2f8da5
prashant-rocky <p3532490@gmail.com>
2025-11-20 11:42:27 +0530

    remove cookies concent

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,4 @@
-// ---------------- Cookie Consent ----------------
-window.addEventListener('load', function () {
-    if (window.cookieconsent) {
-        window.cookieconsent.run({
-            notice_banner_type: "simple",
-            consent_type: "express",
-            palette: "light",
-            language: "en",
-            page_load_consent_levels: ["strictly-necessary"],
-            notice_banner_reject_button_hide: false,
-            preferences_center_close_button_hide: false,
-            page_refresh_confirmation_buttons: false,
-            website_name: "Ichiran Ramen",
-            website_privacy_policy_url: "https://github.com/" // replace with real URL
-        });
-    }
-});
+
 
 // Swiper
 var swiper = new Swiper(".mySwiper", {
